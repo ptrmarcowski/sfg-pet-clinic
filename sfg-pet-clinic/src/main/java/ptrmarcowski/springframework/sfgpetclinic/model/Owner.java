fb5a5b6dac363a87e12f682d3210d1125e2b776b
ptrmarcowski <piotr.marcowski@gmail.com>
2021-01-09 14:43:20 +0100

    Adding POJOs. Closes Issue #2

---
+++ sfg-pet-clinic/src/main/java/ptrmarcowski/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package ptrmarcowski.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ sfg-pet-clinic/src/main/java/ptrmarcowski/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package ptrmarcowski.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
