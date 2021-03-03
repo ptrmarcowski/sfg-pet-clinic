5d122499653268698f8633d4df01bfaa09347b94
ptrmarcowski <piotr.marcowski@gmail.com>
2021-03-03 12:59:42 +0100

    added PetTypeService, closes #29

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ptrmarcowski.springframework.sfgpetclinic.services;
+
+import ptrmarcowski.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
