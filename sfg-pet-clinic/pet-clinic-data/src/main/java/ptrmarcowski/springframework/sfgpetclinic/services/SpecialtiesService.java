8a0242ac043c24f1b208425622d70c1da6a4446d
ptrmarcowski <piotr.marcowski@gmail.com>
2021-03-03 15:01:43 +0100

    Added Specialties Map Impl, closes #31

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package ptrmarcowski.springframework.sfgpetclinic.services;
+
+import ptrmarcowski.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
