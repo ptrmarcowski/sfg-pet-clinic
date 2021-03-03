98d0e1162751f8e92794956618bae4bbed1748c3
ptrmarcowski <piotr.marcowski@gmail.com>
2021-03-03 12:50:00 +0100

    added properties. closes #28

--- sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/model/Owner.java
@@ -4,16 +4,41 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet> pets;
-
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getTelephone() {
+		return telephone;
+	}
+	
 	public Set<Pet> getPets() {
 		return pets;
 	}
-
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
 	}
-	
-	
 	
 }
