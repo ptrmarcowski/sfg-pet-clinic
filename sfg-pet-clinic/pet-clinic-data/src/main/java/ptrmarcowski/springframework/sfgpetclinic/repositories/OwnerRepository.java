9c8eb21a2e328beac086ffb24d55508efca5ec3d
ptrmarcowski <piotr.marcowski@gmail.com>
2021-03-10 11:15:23 +0100

    added spring data jpa service for owners, closes #40

--- sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -6,6 +6,6 @@ import ptrmarcowski.springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
-	
+	Owner findByLastName(String lastName);
 	
 }
