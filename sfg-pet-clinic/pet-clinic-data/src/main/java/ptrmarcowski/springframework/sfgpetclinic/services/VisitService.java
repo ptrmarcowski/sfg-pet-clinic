82f9c67fbcc980382198c0f0296ad7aa65c400ec
ptrmarcowski <piotr.marcowski@gmail.com>
2021-03-12 10:30:20 +0100

    Implemented Map Impl for Visit Service, closes #46

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package ptrmarcowski.springframework.sfgpetclinic.services;
+
+import ptrmarcowski.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+	
+	
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/ptrmarcowski/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package ptrmarcowski.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import ptrmarcowski.springframework.sfgpetclinic.model.Visit;
+import ptrmarcowski.springframework.sfgpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+	//private final
+	
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+		
+		if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+				 || visit.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Ivalid Visit");
+		}
+		
+		return super.save(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		super.delete(object);
+	}
+
+	
+	
+}
